666d86e85ad3d9b07e728b69f3bfcddc5809ef7b
John Kutch <jkutch2@yahoo.com>
2023-04-30 11:56:29 -0400

    rework app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
@@ -32,11 +32,11 @@ function App() {
     <Router>
    <>
      <Navbar />
-     <Switch>
-       <Route exact path='/' component={SearchBooks} />
-       <Route exact path='/saved' component={SavedBooks} />
-       <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-     </Switch>
+     <Routes>
+       <Route path='/' element={<SearchBooks />} />
+       <Route path='/saved' element={<SavedBooks />} />
+       <Route path='*' element={<h1 className='display-2'>Wrong page!</h1>} />
+     </Routes>
    </>
  </Router>
 </ApolloProvider>
